b5b3f2d12a1bc385b2f9760e1aa0744db7523bad
Volodymyr Vozniuk <shopoalo@gmail.com>
2024-11-14 17:37:20 +0200

    add slider

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        dots: false,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        easing: 'ease',
+        infinite: true,
+        initialSlide: 2, 
+        autoplay: false,
+        autoplaySpeed: 300,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover: true,
+        
+        
+        
+
+    });
+});
